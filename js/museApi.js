c66f1de0f9ab04a9841d572c180c153289eb5319
[Shann Roberts] <[dpr.1979@bigpond.com]>
2023-01-14 17:09:22 +0800

    try .find to format API poem

--- js/museApi.js
+++ js/museApi.js
@@ -12,10 +12,15 @@ function fetchPoem() {
     .then((data) => {
         let poemName = data[0].title;
         let poemWriter = data[0].author;
-        let poemPop = data[0].lines;
+        let poemPop = data[0].lines.find(myLines);
+
         document.getElementById("title-poem").innerHTML = poemName;
         document.getElementById("author-poem").innerHTML = poemWriter;
-        document.getElementById("random-poem").innerHTML = poemPop;            
+        document.getElementById("random-poem").innerHTML = poemPop;   
+        
+        function myLines(value, index,array) {
+            return index < 30;
+        };
         console.log(data);
     });
 } 
