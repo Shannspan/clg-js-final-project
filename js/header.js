98f9657a6d78a3b69f4018f2e557625b195b4ace
[Shann Roberts] <[dpr.1979@bigpond.com]>
2023-01-14 11:38:56 +0800

    Remove nav bars

--- js/header.js
+++ js/header.js
@@ -1,6 +1,3 @@
-//maybe header & footer should have there own.js file? Will that fix the error? 
-//Yes it does!! Found info about the "text/javascript" type online and placed in HTML head under the main js <script> link. Both the <script> in head (to access the file) and link to id in the HTML body for location.
-
 class Header extends HTMLElement {
   constructor(){
     super();
@@ -14,14 +11,7 @@ connectedCallback () {
     <div class="title-cutout"><span><sub>A</sub></span> FRINGE OF REEF</div>
     </div>
 
-    <div>
-        <a href="#" class="toggle-nav">
-        <span class="bar"></span>
-        <span class="bar"></span>
-        <span class="bar"></span>
-        </a>
-       
-        <nav class="topnav"> 
+  
 
         <div class="navlinks">
         <ul>
@@ -38,11 +28,11 @@ connectedCallback () {
     `;
 }
 }
-    customElements.define('header-component', Header);
+    // customElements.define('header-component', Header);
 
-    const toggleNav = document.getElementsByClassName("toggle-nav")[0]
+    // const toggleNav = document.getElementsByClassName("toggle-nav")[0]
 
-    const navlinks = document.getElementsByClassName("navlinks")[0]
+    // const navlinks = document.getElementsByClassName("navlinks")[0]
 
 //THIS EVENT LISTENER IS CAUSING AN ERROR 
     // toggleNav.addEventListener('click', () => {
