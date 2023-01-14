ad92f48a50f027066d2e936a802a8900b98da186
[Shann Roberts] <[dpr.1979@bigpond.com]>
2023-01-14 11:49:30 +0800

    revert to heard 12/11/22

--- js/header.js
+++ js/header.js
@@ -1,3 +1,6 @@
+//maybe header & footer should have there own.js file? Will that fix the error? 
+//Yes it does!! Found info about the "text/javascript" type online and placed in HTML head under the main js <script> link. Both the <script> in head (to access the file) and link to id in the HTML body for location.
+
 class Header extends HTMLElement {
   constructor(){
     super();
@@ -10,25 +13,14 @@ connectedCallback () {
     <div class="title-main">
     <div class="title-cutout"><span><sub>A</sub></span> FRINGE OF REEF</div>
     </div>
-
     <div>
-        <a href="#" class="toggle-nav">
-        <span class="bar"></span>
-        <span class="bar"></span>
-        <span class="bar"></span>
-        </a>
-       
-        <nav class="topnav"> 
-
-        <div class="navlinks">
-        <ul>
-        <li><a href="index.html" target ="_blank"">Home</a></li>
-        <li><a href="./beachcombing.html" target="_blank">Play</a></li>      
-        <li><a href="./contact.html" target="_blank">Muse</a></li>
-        <ul>
-    </div>
+    <nav class="topnav">
+    <a class="navlinks" href="index.html" target ="_blank"">Home</a>
+    <a class="navlinks" href="./beachcombing.html" target="_blank">Play</a>   
+    <a class="navlinks" href="#marinelife">Ocean</a>    
+    <a class="navlinks" href="./contact.html" target="_blank">Muse</a>
     </nav>
-    
+    </div>  
     
    
     </header>
@@ -37,14 +29,12 @@ connectedCallback () {
 }
     customElements.define('header-component', Header);
 
-    const toggleNav = document.getElementsByClassName("toggle-nav")[0]
 
-    const navlinks = document.getElementsByClassName("navlinks")[0]
 
-// THIS EVENT LISTENER IS CAUSING AN ERROR 
-//     toggleNav.addEventListener('click', () => {
-//       navlinks.classList.toggle('active')
-//     })
+  
+  
+    
+
 
 
 
