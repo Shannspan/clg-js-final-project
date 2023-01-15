31b8c694681208abdaa80b1fb6209980d882aba9
[Shann Roberts] <[dpr.1979@bigpond.com]>
2023-01-15 20:58:25 +0800

    fix  for API poem? sea theme multi-index array

--- js/museApi.js
+++ js/museApi.js
@@ -1,25 +1,42 @@
-const apiPoem = 'https://poetrydb.org/title/sea/title'
-    console.log(apiPoem);
+const apiPoem = 'https://poetrydb.org/title/sea'
+    //console.log(apiPoem);
 
 const poemBtn = document.getElementById('poem-btn');
 
-poemBtn.addEventListener('click', fetchPoem)
+const click = poemBtn.addEventListener('click', fetchPoem ); 
 
+
+// Below function fetchPoem is returning 'undefined' in console - however it IS working to an extent - what does that mean and how can I refine it to make next index in API array of sea poems appear on click? 
 
 function fetchPoem() {
     fetch(apiPoem)
     .then((response) => response.json())
     .then((data) => {
-        let poemName = data[0].title.body;
-        let poemWriter = data[0].author.body;
-        let poemPop = data[0].lines.body;
+        let poemName = data[0].title;
+        let poemWriter = data[0].author;
+        let poemPop = data[[0]].lines;
 
         document.getElementById("title-poem").innerHTML = poemName;
         document.getElementById("author-poem").innerHTML = poemWriter;
         document.getElementById("random-poem").innerHTML = poemPop;   
         
-        console.log(data);
+        //console.log(data);
     });
 } 
 
-fetchPoem();
+const seaPoem = fetchPoem(); 
+console.log(seaPoem);
+
+const text = document.getElementById("random-poem");
+text.innerHTML = apiPoem[0];
+let counter = 1;
+
+function changeText(){
+    var len = apiPoem.length;
+    if(counter < len){
+      console.log(apiPoem[counter]);
+      text.innerHTML = apiPoem[counter];
+      counter ++;
+    }};
+
+
